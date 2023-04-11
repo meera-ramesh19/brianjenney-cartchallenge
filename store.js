3c37eede6929841ad2194b342b63d35c2ab19d71
meera-ramesh19 <meeraramesh19@outlook.com>
2023-04-11 00:44:43 -0400

    fixed pages

---
+++ store.js
@@ -0,0 +1,11 @@
+let store = JSON.parse(localStorage.getItem("data")) || [];
+/**
+ * DOMCONTENT LOADED
+ * LOAD THE window
+ */
+window.addEventListener('DOMContentLoaded', (event) => {
+ 
+    const cartContainer = document.querySelector('.cart-container');
+    const cartItems = document.querySelectorAll('.cart-item');
+
+});
